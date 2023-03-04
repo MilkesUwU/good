ea562b62997e4a2fc4bedfce5efa0496538b8bab
MilkesUwU <125134463+MilkesUwU@users.noreply.github.com>
2023-03-04 17:59:26 +0300

    Add files via upload

---
+++ MalikaTapaeva.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a={1,2,3}
+b={8,9,7}
+d=a.difference(b)
+print(d)
+print(type(d))
+
+
+# In[9]:
+
+
+#задача№1
+a=['abc','xyz','aba','1221']
+print(a[3:])
+print(type(a))
+
+
+# In[11]:
+
+
+#задача№2
+a=['geen','white','black']
+b=['pink','yellow','red']
+d=a+b
+print(d)
+print(type(d))
+
+
+# In[15]:
+
+
+for i in range(10,5,-1):
+    print(i)
+
+
+# In[16]:
+
+
+sum=0
+for i in range(0,20):
+    sum=sum+i
+print(sum)
+
+
+# In[17]:
+
+
+a=[15,45,68]
+for i in a:
+    print(i*2)
+
+
+# In[19]:
+
+
+a=['red','black','yello','oreng','purpel']
+for i in a:
+    print('coler: '+i)
+
+
+# In[26]:
+
+
+a=0
+while (a<10):
+    a=a+2
+    print(a)
+
+
+# In[29]:
+
+
+d=int(input())
+for i in range(0,d+1):
+    print(i)
+
+
+# In[31]:
+
+
+d=int(input())
+c=int(input())
+for i in range(d,с +1):
+    print(i)
+
+
+# In[32]:
+
+
+d=int(input())
+c=int(input())
+if (d>c):
+    for i in range(d,с):
+        print(i)
+else:
+    for i in range(d,с,-1):
+        print(i)
+
+
+# In[33]:
+
+
+sum=0
+a=int(input())
+for i in range(0,a+1):
+    sum=sum+i
+print(sum)
+
+
+# In[ ]:
+
+
+
+
